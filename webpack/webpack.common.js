371c12d49ee51b56b581bedf490b19882d25599d
mstylski <70129006+mstylski@users.noreply.github.com>
2020-09-03 15:49:22 +0200

    prod build in dist

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     app: Path.resolve(__dirname, '../src/scripts/index.js'),
   },
   output: {
-    path: Path.join(__dirname, '../docs'),
+    path: Path.join(__dirname, '../dist'),
     filename: 'js/[name].js',
   },
   optimization: {
